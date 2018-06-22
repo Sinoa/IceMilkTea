04529aa8f7750660c76b26ef7e9de73d717258e3
mitchydeath <michiwaki.shohei@gmail.com>
2018-06-22 19:30:01 +0900

    barをレンダリングするところまで

--- Program/Runtime/Core/GameMain/SafeGameMain.cs
+++ Program/Runtime/Core/GameMain/SafeGameMain.cs
@@ -13,6 +13,8 @@
 // 2. Altered source versions must be plainly marked as such, and must not be misrepresented as being the original software.
 // 3. This notice may not be removed or altered from any source distribution.
 
+using IceMilkTea.Profiler;
+
 namespace IceMilkTea.Core
 {
     /// <summary>
@@ -21,5 +23,13 @@ namespace IceMilkTea.Core
     /// </summary>
     internal class SafeGameMain : GameMain
     {
+        protected override void Startup()
+        {
+            PerformanceMonitor.Instance.Initialize();
+
+            PerformanceMonitor.Instance.AddProbe(UnityStandardLoopPerformanceProbe.Instance);
+            PerformanceMonitor.Instance.AddRenderer(new GraphicalPerformanceRenderer());
+            PerformanceMonitor.Instance.AddRenderer(new DebugLogPerformanceRenderer());
+        }
     }
 }
--- Program/Runtime/Profiler/RendererImpl/GraphicalPerformanceRenderer.cs
+++ Program/Runtime/Profiler/RendererImpl/GraphicalPerformanceRenderer.cs
@@ -13,6 +13,7 @@
 // 2. Altered source versions must be plainly marked as such, and must not be misrepresented as being the original software.
 // 3. This notice may not be removed or altered from any source distribution.
 
+using System.Linq;
 using UnityEngine;
 
 namespace IceMilkTea.Profiler
@@ -22,12 +23,37 @@ namespace IceMilkTea.Profiler
     /// </summary>
     public class GraphicalPerformanceRenderer : PerformanceRenderer
     {
+        const int FontSize = 16;
+
+        // メンバ変数宣言
+        private UnityStandardLoopProfileResult result;
+        private Font builtinFont;
+        private Material barMaterial;
+        private Material fontMaterial;
+        private Vector2 screenSize;
+
+        public GraphicalPerformanceRenderer()
+        {
+            builtinFont = Resources.Load<Font>("Fonts/FOT-NewCinemaBStd-D");
+            builtinFont.RequestCharactersInTexture("0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ-&_=:;%()[]?{}|/.,", FontSize);
+
+            barMaterial = new Material(Shader.Find("GUI/Text Shader"));
+            fontMaterial = new Material(Shader.Find("UI/Default"));
+
+            screenSize = new Vector2(640, 1280);
+        }
+
         /// <summary>
         /// 出力の準備を行います
         /// </summary>
         /// <param name="profileFetchResults">パフォーマンスモニタから渡されるすべての計測結果の配列</param>
         public override void Begin(ProfileFetchResult[] profileFetchResults)
         {
+            if (result == null)
+            {
+                // プロファイル結果を覚える
+                result = profileFetchResults.First(x => x is UnityStandardLoopProfileResult) as UnityStandardLoopProfileResult;
+            }
         }
 
 
@@ -36,7 +62,9 @@ namespace IceMilkTea.Profiler
         /// </summary>
         public override void End()
         {
+
         }
+
 
 
         /// <summary>
@@ -44,6 +72,75 @@ namespace IceMilkTea.Profiler
         /// </summary>
         public override void Render()
         {
+            var pos = new Vector2(10, 10);
+            var siz = new Vector2(620, 20);
+
+            var npos = new Vector2(pos.x, screenSize.y - pos.y) / screenSize;
+            var nsiz = siz / screenSize;
+
+            var bl = new Vector3(npos.x, npos.y - nsiz.y, 0);
+            var tl = new Vector3(npos.x, npos.y, 0);
+            var tr = new Vector3(npos.x + nsiz.x, npos.y, 0);
+            var br = new Vector3(npos.x + nsiz.x, npos.y - nsiz.y);
+
+
+            CharacterInfo ci;
+            builtinFont.GetCharacterInfo('0', out ci, FontSize);
+
+            GL.PushMatrix();
+            GL.LoadOrtho();
+
+            //builtinFont.material.SetPass(0);
+            barMaterial.SetPass(0);
+            GL.Begin(GL.QUADS);
+
+
+            GL.Color(Color.yellow);
+            GL.TexCoord(Vector3.zero);
+            GL.Vertex(bl);
+
+            GL.Color(Color.yellow);
+            GL.TexCoord(Vector3.zero);
+            GL.Vertex(tl);
+
+            GL.Color(Color.yellow);
+            GL.TexCoord(Vector3.zero);
+            GL.Vertex(tr);
+
+            GL.Color(Color.yellow);
+            GL.TexCoord(Vector3.zero);
+            GL.Vertex(br);
+
+
+            bl.y -= 0.1f;
+            GL.Color(Color.blue);
+            GL.TexCoord(Vector3.zero);
+            GL.Vertex(bl);
+
+            tl.y -= 0.1f;
+            GL.Color(Color.blue);
+            GL.TexCoord(Vector3.zero);
+            GL.Vertex(tl);
+
+            tr.y -= 0.1f;
+            GL.Color(Color.blue);
+            GL.TexCoord(Vector3.zero);
+            GL.Vertex(tr);
+
+            br.y -= 0.1f;
+            GL.Color(Color.blue);
+            GL.TexCoord(Vector3.zero);
+            GL.Vertex(br);
+
+
+
+            GL.End();
+            GL.PopMatrix();
+        }
+
+        private void SetQuads(Vector3 vertex, Color color)
+        {
+
         }
     }
 }
