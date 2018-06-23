491b961def7c98fdcaf8485f2d42a9be2328c470
mitchydeath <michiwaki.shohei@gmail.com>
2018-06-23 21:29:45 +0900

    不要行

--- Program/Runtime/Core/GameMain/SafeGameMain.cs
+++ Program/Runtime/Core/GameMain/SafeGameMain.cs
@@ -13,8 +13,6 @@
 // 2. Altered source versions must be plainly marked as such, and must not be misrepresented as being the original software.
 // 3. This notice may not be removed or altered from any source distribution.
 
-using IceMilkTea.Profiler;
-
 namespace IceMilkTea.Core
 {
     /// <summary>
