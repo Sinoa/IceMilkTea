6bf5d81d2d5482a0ef0a32f48ef840a9e6609c71
Sinoa <sinoa.dev@gmail.com>
2018-07-03 02:00:42 +0900

    [WIP]ServiceManager

---
+++ Program/Runtime/Core/Service/ServiceManager.cs
@@ -0,0 +1,62 @@
+﻿// zlib/libpng License
+//
+// Copyright (c) 2018 Sinoa
+//
+// This software is provided 'as-is', without any express or implied warranty.
+// In no event will the authors be held liable for any damages arising from the use of this software.
+// Permission is granted to anyone to use this software for any purpose,
+// including commercial applications, and to alter it and redistribute it freely,
+// subject to the following restrictions:
+//
+// 1. The origin of this software must not be misrepresented; you must not claim that you wrote the original software.
+//    If you use this software in a product, an acknowledgment in the product documentation would be appreciated but is not required.
+// 2. Altered source versions must be plainly marked as such, and must not be misrepresented as being the original software.
+// 3. This notice may not be removed or altered from any source distribution.
+
+using System.Collections.Generic;
+
+namespace IceMilkTea.Core
+{
+    /// <summary>
+    /// IceMilkTeaのサービスを管理及び制御を行うクラスです
+    /// </summary>
+    public sealed class ServiceManager
+    {
+        // メンバ変数定義
+        private List<ServiceProvider> serviceList;
+
+
+
+        /// <summary>
+        /// ServiceManagerの初期化を行います
+        /// </summary>
+        internal ServiceManager()
+        {
+            serviceList = new List<ServiceProvider>();
+        }
+
+
+        internal void Update()
+        {
+            throw new System.NotImplementedException();
+        }
+
+
+        public T AddService<T>()
+        {
+            throw new System.NotImplementedException();
+        }
+
+
+        public T GetService<T>()
+        {
+            throw new System.NotImplementedException();
+        }
+
+
+        public void RemoveService<T>()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
---
+++ Program/Runtime/Core/Service/ServiceProvider.cs
@@ -0,0 +1,34 @@
+﻿// zlib/libpng License
+//
+// Copyright (c) 2018 Sinoa
+//
+// This software is provided 'as-is', without any express or implied warranty.
+// In no event will the authors be held liable for any damages arising from the use of this software.
+// Permission is granted to anyone to use this software for any purpose,
+// including commercial applications, and to alter it and redistribute it freely,
+// subject to the following restrictions:
+//
+// 1. The origin of this software must not be misrepresented; you must not claim that you wrote the original software.
+//    If you use this software in a product, an acknowledgment in the product documentation would be appreciated but is not required.
+// 2. Altered source versions must be plainly marked as such, and must not be misrepresented as being the original software.
+// 3. This notice may not be removed or altered from any source distribution.
+
+using System;
+
+namespace IceMilkTea.Core
+{
+    [Flags]
+    public enum ServiceUpdateTiming: uint
+    {
+        MainLoopHead = 0x00000001,
+        MainLoopTail = 0x00000002,
+        PreFixedUpdate = 0x00000004,
+        PostFixedUpdate = 0x00000008,
+    }
+
+
+
+    public abstract class ServiceProvider
+    {
+    }
+}
