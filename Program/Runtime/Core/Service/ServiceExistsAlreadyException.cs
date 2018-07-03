39f8f02dfba17e88fa24297b07d725333aed9f1e
Sinoa <sinoa.dev@gmail.com>
2018-07-03 14:48:44 +0900

    サービスが既に存在している例外クラスも実装

---
+++ Program/Runtime/Core/Service/ServiceExistsAlreadyException.cs
@@ -0,0 +1,33 @@
+﻿// zlib/libpng License
+//
+// Copyright (c) 2018 Sinoa
+//
+// This software is provided 'as-is', without any express or implied warranty.
+// In no event will the authors be held liable for any damages arising from the use of this software.
+// Permission is granted to anyone to use this software for any purpose,
+// including commercial applications, and to alter it and redistribute it freely,
+// subject to the following restrictions:
+//
+// 1. The origin of this software must not be misrepresented; you must not claim that you wrote the original software.
+//    If you use this software in a product, an acknowledgment in the product documentation would be appreciated but is not required.
+// 2. Altered source versions must be plainly marked as such, and must not be misrepresented as being the original software.
+// 3. This notice may not be removed or altered from any source distribution.
+
+using System;
+
+namespace IceMilkTea.Core
+{
+    /// <summary>
+    /// サービスが既に存在している場合にスローされる例外クラスです
+    /// </summary>
+    public class ServiceExistsAlreadyException : IceMilkTeaException
+    {
+        /// <summary>
+        /// ServiceExistsAlreadyException インスタンスの初期化をします
+        /// </summary>
+        /// <param name="target">既に存在しているサービスのタイプ</param>
+        public ServiceExistsAlreadyException(Type target) : base($"This {target.Name} service is exists already.")
+        {
+        }
+    }
+}
