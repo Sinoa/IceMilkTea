111753fe835f67d43cf7d6047a70c54721a3b1ec
Sinoa <sinoa.dev@gmail.com>
2018-07-03 15:07:23 +0900

    実装予定の関数にコメントを付けた

--- Program/Runtime/Core/Service/ServiceManager.cs
+++ Program/Runtime/Core/Service/ServiceManager.cs
@@ -32,31 +32,75 @@ namespace IceMilkTea.Core
         /// </summary>
         internal ServiceManager()
         {
+            // メンバの初期化をする
             serviceList = new List<ServiceProvider>();
         }
 
 
-        internal void Update()
+        #region リスト操作系
+        /// <summary>
+        /// 指定されたサービスプロバイダの追加をします。
+        /// また、サービスプロバイダの型が同じインスタンスが存在する場合は例外がスローされます。
+        /// </summary>
+        /// <param name="service">追加するサービスプロバイダのインスタンス</param>
+        /// <exception cref="ServiceExistsAlreadyException">既に同じ型のサービスプロバイダが追加されています</exception>
+        public void AddService(ServiceProvider service)
+        {
+        }
+
+
+        /// <summary>
+        /// 指定されたサービスプロバイダの追加をします。
+        /// この関数は AddService() 関数と違い、同じ型のサービスの追加は出来ませんが、例外をスローしません。
+        /// </summary>
+        /// <param name="service">追加するサービスプロバイダのインスタンス</param>
+        /// <returns>サービスの追加が出来た場合は true を、出来なかった場合は false を返します</returns>
+        public bool TryAddService(ServiceProvider service)
         {
             throw new System.NotImplementedException();
         }
 
 
-        public T AddService<T>()
-        {
-            throw new System.NotImplementedException();
-        }
-
-
+        /// <summary>
+        /// 指定された型のサービスプロバイダを取得します。
+        /// また、サービスプロバイダが見つけられなかった場合は例外がスローされます。
+        /// </summary>
+        /// <typeparam name="T">取得するサービスプロバイダの型</typeparam>
+        /// <returns>見つけられたサービスプロバイダのインスタンスを返します</returns>
+        /// <exception cref="ServiceNotFoundException">指定された型のサービスプロバイダが見つかりませんでした</exception>
         public T GetService<T>()
         {
             throw new System.NotImplementedException();
         }
 
 
-        public void RemoveService<T>()
+        /// <summary>
+        /// 指定された型のサービスプロバイダを取得します
+        /// </summary>
+        /// <typeparam name="T">取得するサービスプロバイダの型</typeparam>
+        /// <param name="service">見つけられたサービスプロバイダのインスタンスを設定しますが、見つけられなかった場合はnullが設定されます</param>
+        /// <returns>サービスを取得できた場合は true を、出来なかった場合は false を返します</returns>
+        public bool TryGetService<T>(out T service)
         {
             throw new System.NotImplementedException();
         }
+
+
+        /// <summary>
+        /// 指定された型のサービスプロバイダを削除します
+        /// </summary>
+        /// <typeparam name="T">削除するサービスプロバイダの型</typeparam>
+        public void RemoveService<T>()
+        {
+        }
+
+
+        /// <summary>
+        /// サービスマネージャが保持しているすべてのサービスプロバイダを削除します
+        /// </summary>
+        internal void RemoveAllService()
+        {
+        }
+        #endregion
     }
 }
