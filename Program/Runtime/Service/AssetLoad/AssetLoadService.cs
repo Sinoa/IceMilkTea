7ca1f93c081f0669b7c29b19849355872ca199f2
Sinoa <sinoa.dev@gmail.com>
2018-08-31 15:08:00 +0900

    必要になりそうなクラスの列挙実装だけ

--- Program/Runtime/Service/AssetLoad/AssetLoadService.cs
+++ Program/Runtime/Service/AssetLoad/AssetLoadService.cs
@@ -15,15 +15,16 @@
 
 using System;
 using System.Collections.Generic;
-using System.Text;
 using IceMilkTea.Core;
 using UnityEngine;
 
 namespace IceMilkTea.Service
 {
-    // Unityのアセットの基底型になるObjectは、SystemのObjectとややこしくなるのでここではUnityAssetと名付ける（WeakReference版も定義）
+    // Unityのアセットの基底型になるObjectは、SystemのObjectとややこしくなるので
+    // ここではUnityAssetと名付ける（WeakReference版も定義）
     using UnityAsset = UnityEngine.Object;
     using WeakUnityAsset = WeakReference<UnityEngine.Object>;
+
 
 
     /// <summary>
@@ -31,28 +32,110 @@ namespace IceMilkTea.Service
     /// </summary>
     public class AssetLoadService : GameService
     {
-        private Crc64Base crc;
-        private Encoding utf8;
-        private Dictionary<ulong, AssetBundle> assetBundleCache;
+    }
+
+
+
+    /// <summary>
+    /// 読み込まれたアセットのキャッシュを貯蔵するクラスです
+    /// </summary>
+    internal class AssetCacheStorage
+    {
+        // メンバ変数定義
         private Dictionary<ulong, WeakUnityAsset> assetCacheTable;
 
 
 
-        public IAwaitable<UnityAsset> LoadAsync()
+        /// <summary>
+        /// 指定されたアセットIDから、キャッシュ済みアセットの取得をします。
+        /// </summary>
+        /// <typeparam name="TAssetType">取得するアセットの型</typeparam>
+        /// <param name="assetId">取得するキャッシュのアセットID</param>
+        /// <returns>キャッシュ済みのアセットが存在する場合は、そのインスタンスを返しますが、キャッシュがない場合は null を返します</returns>
+        public TAssetType GetAssetCache<TAssetType>(ulong assetId) where TAssetType : UnityAsset
         {
-            return new ImtTask<UnityAsset>(_ => null);
+            // まずはアセットIDから参照を引っ張り出して存在しないなら
+            WeakUnityAsset assetReference;
+            if (!assetCacheTable.TryGetValue(assetId, out assetReference))
+            {
+                // まだキャッシュされていない
+                return null;
+            }
+
+
+            // 参照から実体の取得が出来なかったら
+            UnityAsset asset;
+            if (!assetReference.TryGetTarget(out asset))
+            {
+                // レコードを削除してキャッシュが取り出せなかったとして返す
+                assetCacheTable.Remove(assetId);
+                return null;
+            }
+
+
+            // 取り出した参照をキャストして返す
+            return (TAssetType)asset;
+        }
+
+
+        /// <summary>
+        /// 指定されたアセットIDとして、アセットを貯蔵します。
+        /// もし既に貯蔵済みの場合は、以前のアセットの参照は破棄されます。
+        /// </summary>
+        /// <param name="assetId">貯蔵するアセットのアセットID</param>
+        /// <param name="asset">貯蔵するアセット</param>
+        /// <exception cref="ArgumentNullException">asset が null です</exception>
+        public void StoreAssetCache(ulong assetId, UnityAsset asset)
+        {
+            // もしassetがnullなら
+            if (asset == null)
+            {
+                // 何を貯蔵するんですか
+                throw new ArgumentNullException(nameof(asset));
+            }
+
+
+            // 一度テーブルから参照を引っ張り出せるか試みる
+            WeakUnityAsset assetReference;
+            if (assetCacheTable.TryGetValue(assetId, out assetReference))
+            {
+                // 取り出せたのなら参照を上書きして終了
+                assetReference.SetTarget(asset);
+                return;
+            }
         }
     }
 
 
 
-    internal class AssetBundleContainer
+    /// <summary>
+    /// アセットローダーを提供するクラスです
+    /// </summary>
+    internal class AssetLoaderProvider
     {
+        public AssetLoader RequestAssetLoader(ulong assetId, Uri assetUrl)
+        {
+            throw new NotImplementedException();
+        }
     }
 
 
 
-    internal class AssetCacheController
+    /// <summary>
+    /// アセットパスから適切なローダーを解決するリゾルバクラスです
+    /// </summary>
+    public abstract class AssetPathResolver
     {
+        public abstract AssetLoader GetLoader(ulong assetId, Uri assetUrl);
+    }
+
+
+
+    /// <summary>
+    /// アセットのロードを実際に行うクラスです
+    /// </summary>
+    public abstract class AssetLoader
+    {
+        public abstract IAwaitable<UnityAsset> LoadAssetAsync(ulong assetId, Uri assetUrl);
     }
 }
