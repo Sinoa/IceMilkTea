a7347aa19fa6e7cbb1f21db326ed0e079fbdf6b0
Sinoa <sinoa.dev@gmail.com>
2018-10-12 20:58:17 +0900

    マニフェストのストレージも実装

--- Program/Runtime/Service/AssetManagement/SubSystem/AssetBundleManifestStorage/ImtSimpleAssetBundleManifestStorage.cs
+++ Program/Runtime/Service/AssetManagement/SubSystem/AssetBundleManifestStorage/ImtSimpleAssetBundleManifestStorage.cs
@@ -18,6 +18,7 @@ using System.Collections.Generic;
 using System.IO;
 using System.Threading.Tasks;
 using IceMilkTea.Core;
+using UnityEngine;
 
 namespace IceMilkTea.Service
 {
@@ -26,6 +27,22 @@ namespace IceMilkTea.Service
     /// </summary>
     public class ImtSimpleAssetBundleManifestStorage : AssetBundleManifestStorage
     {
+        /// <summary>
+        /// マニフェストの管理情報を保持したクラスです
+        /// </summary>
+        private class ManifestManagementInfo
+        {
+            /// <summary>
+            /// 管理しているマニフェスト名の配列
+            /// </summary>
+            public string[] ManifestNames;
+        }
+
+
+
+        // 定数定義
+        private const string ManifestManagementInfoFileName = "ImtManifestInfo.json";
+
         // メンバ変数定義
         private DirectoryInfo baseDirectoryInfo;
         private Dictionary<string, ImtAssetBundleManifest> manifestTable;
@@ -79,7 +96,21 @@ namespace IceMilkTea.Service
             }
 
 
-            throw new NotImplementedException();
+            // テーブルに存在するキーを配列として返す
+            var names = new string[manifestTable.Count];
+            manifestTable.Keys.CopyTo(names, 0);
+            return names;
+        }
+
+
+        /// <summary>
+        /// マニフェスト管理情報ファイルの情報を取得します
+        /// </summary>
+        /// <returns>マニフェスト管理情報ファイルのインスタンスを返します</returns>
+        private FileInfo GetManifestManagementInfoFileInfo()
+        {
+            // ベースパスとファイル名を結合したパスでファイル情報を生成して返す
+            return new FileInfo(Path.Combine(baseDirectoryInfo.FullName, ManifestManagementInfoFileName));
         }
 
 
@@ -91,7 +122,53 @@ namespace IceMilkTea.Service
         /// <returns>マニフェストを非同期でロードしているタスクを返します</returns>
         public override Task LoadAsync(IProgress<float> progress)
         {
-            throw new NotImplementedException();
+            // ベースディレクトリ情報を更新して、ディレクトリが存在しないなら
+            baseDirectoryInfo.Refresh();
+            if (!baseDirectoryInfo.Exists)
+            {
+                // そもそもロードするものがないとして終了
+                return Task.CompletedTask;
+            }
+
+
+            // マニフェスト管理情報ファイル情報を取得して存在しないなら
+            var manifestManagementInfoFileInfo = GetManifestManagementInfoFileInfo();
+            manifestManagementInfoFileInfo.Refresh();
+            if (!manifestManagementInfoFileInfo.Exists)
+            {
+                // 読み込むものがないので終了
+                return Task.CompletedTask;
+            }
+
+
+            // 全体を非同期タスクとして動かす
+            return Task.Factory.StartNew(async () =>
+            {
+                // マニフェスト管理情報ファイルを開く
+                var manifestManagementInfo = default(ManifestManagementInfo);
+                using (var fileStream = new StreamReader(File.OpenRead(manifestManagementInfoFileInfo.FullName)))
+                {
+                    // Jsonデシリアライズして読み込む
+                    manifestManagementInfo = JsonUtility.FromJson<ManifestManagementInfo>(await fileStream.ReadToEndAsync());
+                }
+
+
+                // マニフェストの名前の数分回る
+                foreach (var manifestName in manifestManagementInfo.ManifestNames)
+                {
+                    // マニフェストの名前でマニフェストファイル名を生成して開く
+                    var manifestFilePath = Path.Combine(baseDirectoryInfo.FullName, manifestName + ".json");
+                    using (var fileStream = new StreamReader(File.OpenRead(manifestFilePath)))
+                    {
+                        // jsonデータを非同期で読み込む
+                        var manifest = JsonUtility.FromJson<ImtAssetBundleManifest>(await fileStream.ReadToEndAsync());
+
+
+                        // テーブルに読み込んだマニフェストを設定する
+                        SetManifest(ref manifest);
+                    }
+                }
+            });
         }
 
 
@@ -102,7 +179,54 @@ namespace IceMilkTea.Service
         /// <returns>マニフェストを非同期で永続化しているタスクを返します</returns>
         public override Task SaveAsync(IProgress<float> progress)
         {
-            throw new NotImplementedException();
+            // 全体を非同期タスクとして動かす
+            return Task.Factory.StartNew(async () =>
+            {
+                // ベースディレクトリ情報を更新して、ディレクトリが存在しないなら
+                baseDirectoryInfo.Refresh();
+                if (!baseDirectoryInfo.Exists)
+                {
+                    // ディレクトリを生成する
+                    baseDirectoryInfo.Create();
+                }
+
+
+                // マニフェスト管理情報を生成する
+                var manifestManagementInfo = new ManifestManagementInfo();
+                manifestManagementInfo.ManifestNames = new string[manifestTable.Count];
+
+
+                // テーブルに含まれるレコード分回る
+                int loopCount = 0;
+                foreach (var manifest in manifestTable.Values)
+                {
+                    // マニフェストの名前を覚えてjsonシリアライズをする
+                    manifestManagementInfo.ManifestNames[loopCount] = manifest.Name;
+                    var jsonData = JsonUtility.ToJson(manifest);
+
+
+                    // マニフェストの名前でマニフェストファイル名を生成して開く
+                    var manifestFilePath = Path.Combine(baseDirectoryInfo.FullName, manifest.Name + ".json");
+                    using (var fileStream = new StreamWriter(File.OpenWrite(manifestFilePath)))
+                    {
+                        // jsonデータを非同期で書き込む
+                        await fileStream.WriteAsync(jsonData);
+                    }
+
+
+                    // ループカウントをインクリメントして進捗通知
+                    progress.Report((float)++loopCount / manifestTable.Count);
+                }
+
+
+                // 最後にマニフェスト情報を保存する
+                var manifestManagementInfoJsonData = JsonUtility.ToJson(manifestManagementInfo);
+                using (var fileStream = new StreamWriter(File.OpenWrite(GetManifestManagementInfoFileInfo().FullName)))
+                {
+                    // 非同期で書き込む
+                    await fileStream.WriteAsync(manifestManagementInfoJsonData);
+                }
+            });
         }
 
 
@@ -112,7 +236,8 @@ namespace IceMilkTea.Service
         /// <param name="manifest">管理対象となるマニフェストの参照</param>
         public override void SetManifest(ref ImtAssetBundleManifest manifest)
         {
-            throw new NotImplementedException();
+            // マニフェストテーブルに設定する
+            manifestTable[manifest.Name] = manifest;
         }
 
 
@@ -122,9 +247,19 @@ namespace IceMilkTea.Service
         /// <param name="name">取得したいマニフェストの名前</param>
         /// <param name="manifest">取得したマニフェストを受け取る変数</param>
         /// <returns>マニフェストの取得に成功した場合は true を、失敗した場合は false を返します</returns>
+        /// <exception cref="ArgumentNullException">name が null です</exception>
         public override bool TryGetManifest(string name, out ImtAssetBundleManifest manifest)
         {
-            throw new NotImplementedException();
+            // nullを渡されたら
+            if (name == null)
+            {
+                // null では引っ張り出せない
+                throw new ArgumentNullException(nameof(name));
+            }
+
+
+            // 名前から取得した結果をそのまま帰す
+            return manifestTable.TryGetValue(name, out manifest);
         }
     }
 }
