258f03c108351238b21ec73dcd38741d7915775a
eiki.kudaka <eiki.kudaka@gu3.co.jp>
2019-07-09 17:26:37 +0900

    contain animator processing time

--- Program/Runtime/Service/Profiler/ProbeImpl/UnityStandardLoopPerformanceProbe.cs
+++ Program/Runtime/Service/Profiler/ProbeImpl/UnityStandardLoopPerformanceProbe.cs
@@ -104,8 +104,8 @@ namespace IceMilkTea.Profiler
             rootLoopSystem.InsertLoopSystem<FixedUpdate.ScriptRunDelayedFixedFrameRate, FixedUpdateProbeEnd>(InsertTiming.AfterInsert, () => fixedUpdateEndCount = stopwatch.ElapsedTicks);
             rootLoopSystem.InsertLoopSystem<Update.ScriptRunBehaviourUpdate, UpdateProbeStart>(InsertTiming.BeforeInsert, () => updateStartCount = stopwatch.ElapsedTicks);
             rootLoopSystem.InsertLoopSystem<Update.ScriptRunBehaviourUpdate, UpdateProbeEnd>(InsertTiming.AfterInsert, () => updateEndCount = stopwatch.ElapsedTicks);
-            rootLoopSystem.InsertLoopSystem<PreLateUpdate.ScriptRunBehaviourLateUpdate, LateUpdateProbeStart>(InsertTiming.BeforeInsert, () => lateUpdateStartCount = stopwatch.ElapsedTicks);
-            rootLoopSystem.InsertLoopSystem<PreLateUpdate.ScriptRunBehaviourLateUpdate, LateUpdateProbeEnd>(InsertTiming.AfterInsert, () => lateUpdateEndCount = stopwatch.ElapsedTicks);
+            rootLoopSystem.InsertLoopSystem<PreLateUpdate.AIUpdatePostScript, LateUpdateProbeStart>(InsertTiming.BeforeInsert, () => lateUpdateStartCount = stopwatch.ElapsedTicks);
+            rootLoopSystem.InsertLoopSystem<PreLateUpdate.ConstraintManagerUpdate, LateUpdateProbeEnd>(InsertTiming.AfterInsert, () => lateUpdateEndCount = stopwatch.ElapsedTicks);
             rootLoopSystem.BuildAndSetUnityPlayerLoop();
 
 
