daed6fa59c2f59b4f67931a7d4323c9a7fbc72fa
Sinoa <sinoa.dev@gmail.com>
2019-09-26 19:19:36 +0900

    再試行制御可能なクラスを実装

---
+++ Program/Runtime/Core/UnitCode/PureCsharp/RetryableWorker.cs
@@ -0,0 +1,223 @@
+﻿// zlib/libpng License
+//
+// Copyright (c) 2019 Sinoa
+//
+// This software is provided 'as-is', without any express or implied warranty.
+// In no event will the authors be held liable for any damages arising from the use of this software.
+// Permission is granted to anyone to use this software for any purpose,
+// including commercial applications, and to alter it and redistribute it freely,
+// subject to the following restrictions:
+//
+// 1. The origin of this software must not be misrepresented; you must not claim that you wrote the original software.
+//    If you use this software in a product, an acknowledgment in the product documentation would be appreciated but is not required.
+// 2. Altered source versions must be plainly marked as such, and must not be misrepresented as being the original software.
+// 3. This notice may not be removed or altered from any source distribution.
+
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace IceMilkTea.Core
+{
+    /// <summary>
+    /// 再試行処理可能なワーカー抽象クラスです
+    /// </summary>
+    public abstract class RetryableWorker
+    {
+        // クラス変数宣言
+        private static readonly Progress<int> EmptyProgress;
+
+
+
+        /// <summary>
+        /// RetryableWorker クラスの初期化をします
+        /// </summary>
+        static RetryableWorker()
+        {
+            // 空進捗通知オブジェクトを生成しておく
+            EmptyProgress = new Progress<int>();
+        }
+
+
+
+        #region DoWork関数群
+        /// <summary>
+        /// 指定された関数を実行します
+        /// </summary>
+        /// <param name="work">実行する同期関数</param>
+        /// <returns>指定された関数を実行しているタスクを返します</returns>
+        /// <exception cref="ArgumentNullException">work が null です</exception>
+        public Task DoWork(Action work)
+        {
+            // 空進捗オブジェクトでキャンセルなしで同じ関数を呼ぶ
+            return DoWork(work, EmptyProgress, CancellationToken.None);
+        }
+
+
+        /// <summary>
+        /// 指定された非同期関数を実行します
+        /// </summary>
+        /// <param name="work">実行する同期関数</param>
+        /// <returns>指定された関数を実行しているタスクを返します</returns>
+        /// <exception cref="ArgumentNullException">work が null です</exception>
+        public Task DoWork(Task work)
+        {
+            // 空進捗オブジェクトでキャンセルなしで同じ関数を呼ぶ
+            return DoWork(work, EmptyProgress, CancellationToken.None);
+        }
+
+
+        /// <summary>
+        /// 指定された値を返す関数を実行します
+        /// </summary>
+        /// <typeparam name="TResult">返す値の型</typeparam>
+        /// <param name="work">実行する同期関数</param>
+        /// <returns>指定された関数を実行しているタスクを返します</returns>
+        /// <exception cref="ArgumentNullException">work が null です</exception>
+        public Task<TResult> DoWork<TResult>(Func<TResult> work)
+        {
+            // 空進捗オブジェクトでキャンセルなしで同じ関数を呼ぶ
+            return DoWork(work, EmptyProgress, CancellationToken.None);
+        }
+
+
+        /// <summary>
+        /// 指定された値を返す非同期関数を実行します
+        /// </summary>
+        /// <typeparam name="TResult">返す値の型</typeparam>
+        /// <param name="work">実行する非同期関数</param>
+        /// <returns>指定された関数を実行しているタスクを返します</returns>
+        /// <exception cref="ArgumentNullException">work が null です</exception>
+        public Task<TResult> DoWork<TResult>(Task<TResult> work)
+        {
+            // 空進捗オブジェクトでキャンセルなしで同じ関数を呼ぶ
+            return DoWork(work, EmptyProgress, CancellationToken.None);
+        }
+
+
+        /// <summary>
+        /// 指定された関数を実行します
+        /// </summary>
+        /// <param name="work">実行する同期関数</param>
+        /// <param name="progress">リトライ時の進捗通知オブジェクト。既定は空通知オブジェクトです。</param>
+        /// <returns>指定された関数を実行しているタスクを返します</returns>
+        /// <exception cref="ArgumentNullException">work が null です</exception>
+        /// <exception cref="ArgumentNullException">progress が null です</exception>
+        public Task DoWork(Action work, IProgress<int> progress)
+        {
+            // キャンセルなしで同じ関数を呼ぶ
+            return DoWork(work, progress, CancellationToken.None);
+        }
+
+
+        /// <summary>
+        /// 指定された非同期関数を実行します
+        /// </summary>
+        /// <param name="work">実行する同期関数</param>
+        /// <param name="progress">リトライ時の進捗通知オブジェクト。既定は空通知オブジェクトです。</param>
+        /// <returns>指定された関数を実行しているタスクを返します</returns>
+        /// <exception cref="ArgumentNullException">work が null です</exception>
+        /// <exception cref="ArgumentNullException">progress が null です</exception>
+        public Task DoWork(Task work, IProgress<int> progress)
+        {
+            // キャンセルなしで同じ関数を呼ぶ
+            return DoWork(work, progress, CancellationToken.None);
+        }
+
+
+        /// <summary>
+        /// 指定された値を返す関数を実行します
+        /// </summary>
+        /// <typeparam name="TResult">返す値の型</typeparam>
+        /// <param name="work">実行する同期関数</param>
+        /// <param name="progress">リトライ時の進捗通知オブジェクト。既定は空通知オブジェクトです。</param>
+        /// <returns>指定された関数を実行しているタスクを返します</returns>
+        /// <exception cref="ArgumentNullException">work が null です</exception>
+        /// <exception cref="ArgumentNullException">progress が null です</exception>
+        public Task<TResult> DoWork<TResult>(Func<TResult> work, IProgress<int> progress)
+        {
+            // キャンセルなしで同じ関数を呼ぶ
+            return DoWork(work, progress, CancellationToken.None);
+        }
+
+
+
+        /// <summary>
+        /// 指定された値を返す非同期関数を実行します
+        /// </summary>
+        /// <typeparam name="TResult">返す値の型</typeparam>
+        /// <param name="work">実行する非同期関数</param>
+        /// <param name="progress">リトライ時の進捗通知オブジェクト。既定は空通知オブジェクトです。</param>
+        /// <returns>指定された関数を実行しているタスクを返します</returns>
+        /// <exception cref="ArgumentNullException">work が null です</exception>
+        /// <exception cref="ArgumentNullException">progress が null です</exception>
+        public Task<TResult> DoWork<TResult>(Task<TResult> work, IProgress<int> progress)
+        {
+            // キャンセルなしで同じ関数を呼ぶ
+            return DoWork(work, progress, CancellationToken.None);
+        }
+
+
+        /// <summary>
+        /// 指定された関数を実行します
+        /// </summary>
+        /// <param name="work">実行する同期関数</param>
+        /// <param name="progress">リトライ時の進捗通知オブジェクト。既定は空通知オブジェクトです。</param>
+        /// <param name="cancellationToken">リトライのキャンセル要求を監視するためのトークン。既定は None です。</param>
+        /// <returns>指定された関数を実行しているタスクを返します</returns>
+        /// <exception cref="ArgumentNullException">work が null です</exception>
+        /// <exception cref="ArgumentNullException">progress が null です</exception>
+        public Task DoWork(Action work, IProgress<int> progress, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+
+
+        /// <summary>
+        /// 指定された非同期関数を実行します
+        /// </summary>
+        /// <param name="work">実行する同期関数</param>
+        /// <param name="progress">リトライ時の進捗通知オブジェクト。既定は空通知オブジェクトです。</param>
+        /// <param name="cancellationToken">リトライのキャンセル要求を監視するためのトークン。既定は None です。</param>
+        /// <returns>指定された関数を実行しているタスクを返します</returns>
+        /// <exception cref="ArgumentNullException">work が null です</exception>
+        /// <exception cref="ArgumentNullException">progress が null です</exception>
+        public Task DoWork(Task work, IProgress<int> progress, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+
+
+        /// <summary>
+        /// 指定された値を返す関数を実行します
+        /// </summary>
+        /// <typeparam name="TResult">返す値の型</typeparam>
+        /// <param name="work">実行する同期関数</param>
+        /// <param name="progress">リトライ時の進捗通知オブジェクト。既定は空通知オブジェクトです。</param>
+        /// <param name="cancellationToken">リトライのキャンセル要求を監視するためのトークン。既定は None です。</param>
+        /// <returns>指定された関数を実行しているタスクを返します</returns>
+        /// <exception cref="ArgumentNullException">work が null です</exception>
+        /// <exception cref="ArgumentNullException">progress が null です</exception>
+        public Task<TResult> DoWork<TResult>(Func<TResult> work, IProgress<int> progress, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+
+
+        /// <summary>
+        /// 指定された値を返す非同期関数を実行します
+        /// </summary>
+        /// <typeparam name="TResult">返す値の型</typeparam>
+        /// <param name="work">実行する非同期関数</param>
+        /// <param name="progress">リトライ時の進捗通知オブジェクト。既定は空通知オブジェクトです。</param>
+        /// <param name="cancellationToken">リトライのキャンセル要求を監視するためのトークン。既定は None です。</param>
+        /// <returns>指定された関数を実行しているタスクを返します</returns>
+        /// <exception cref="ArgumentNullException">work が null です</exception>
+        /// <exception cref="ArgumentNullException">progress が null です</exception>
+        public Task<TResult> DoWork<TResult>(Task<TResult> work, IProgress<int> progress, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+        #endregion
+    }
+}
