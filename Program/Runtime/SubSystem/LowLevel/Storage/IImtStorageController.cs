ce222f690fd6720e4e06a99e8568c9dd3b7068c8
Sinoa <sinoa.dev@gmail.com>
2019-09-30 12:45:55 +0900

    ファイルの移動とImtStorageの本格実装

---
+++ Program/Runtime/SubSystem/LowLevel/Storage/IImtStorageController.cs
@@ -0,0 +1,66 @@
+﻿// zlib/libpng License
+//
+// Copyright (c) 2019 Sinoa
+//
+// This software is provided 'as-is', without any express or implied warranty.
+// In no event will the authors be held liable for any damages arising from the use of this software.
+// Permission is granted to anyone to use this software for any purpose,
+// including commercial applications, and to alter it and redistribute it freely,
+// subject to the following restrictions:
+//
+// 1. The origin of this software must not be misrepresented; you must not claim that you wrote the original software.
+//    If you use this software in a product, an acknowledgment in the product documentation would be appreciated but is not required.
+// 2. Altered source versions must be plainly marked as such, and must not be misrepresented as being the original software.
+// 3. This notice may not be removed or altered from any source distribution.
+
+using System;
+using System.IO;
+
+namespace IceMilkTea.SubSystem
+{
+    /// <summary>
+    /// 実際のストリーム操作を提供するストレージコントローラインターフェイスです
+    /// </summary>
+    public interface IImtStorageController
+    {
+        /// <summary>
+        /// ストレージ名を取得します
+        /// </summary>
+        string Name { get; }
+
+
+
+        /// <summary>
+        /// 指定されたURIのデータが存在するか否かを確認します
+        /// </summary>
+        /// <param name="uri">存在を確認するURI</param>
+        /// <returns>データが存在する場合は true を、存在しない場合は false を返します</returns>
+        bool Exists(Uri uri);
+
+
+        /// <summary>
+        /// 指定されたURIのストリームを開きます
+        /// </summary>
+        /// <param name="uri">開く対象となるURI</param>
+        /// <param name="mode">ストリームを開くモード</param>
+        /// <param name="access">ストリームへのアクセス方法</param>
+        /// <param name="share">ストリームの共有設定</param>
+        /// <param name="cacheSize">ストリームが持つキャッシュサイズ</param>
+        /// <param name="useAsync">ストリームの非同期操作を使用するか否か</param>
+        /// <returns>正しくストリームを開けた場合はストリームを返しますが、開けなかった場合は null を返します</returns>
+        Stream Open(Uri uri, FileMode mode, FileAccess access, FileShare share, int cacheSize, bool useAsync);
+
+
+        /// <summary>
+        /// 指定されたURIのデータを削除します
+        /// </summary>
+        /// <param name="uri">削除するURI</param>
+        void Delete(Uri uri);
+
+
+        /// <summary>
+        /// 管理しているデータすべてを削除します
+        /// </summary>
+        void DeleteAll();
+    }
+}
---
+++ Program/Runtime/SubSystem/LowLevel/Storage/ImtStorageUriExtensions.cs
@@ -0,0 +1,106 @@
+﻿// zlib/libpng License
+//
+// Copyright (c) 2019 Sinoa
+//
+// This software is provided 'as-is', without any express or implied warranty.
+// In no event will the authors be held liable for any damages arising from the use of this software.
+// Permission is granted to anyone to use this software for any purpose,
+// including commercial applications, and to alter it and redistribute it freely,
+// subject to the following restrictions:
+//
+// 1. The origin of this software must not be misrepresented; you must not claim that you wrote the original software.
+//    If you use this software in a product, an acknowledgment in the product documentation would be appreciated but is not required.
+// 2. Altered source versions must be plainly marked as such, and must not be misrepresented as being the original software.
+// 3. This notice may not be removed or altered from any source distribution.
+
+using System;
+
+namespace IceMilkTea.SubSystem
+{
+    /// <summary>
+    /// ImtStorage によるURIクラスの拡張関数実装用クラスです
+    /// </summary>
+    public static class ImtStorageUriExtensions
+    {
+        // 定数定義
+        public const string SchemeAssetBundle = "assetbundle";
+        public const string SchemeAssetCatalog = "assetcatalog";
+        public const string SchemeAssetDatabase = "assetdatabase";
+        public const string SchemeStreammingAssets = "streammingassets";
+        public const string SchemeResources = "resources";
+        public const string SchemeTemporary = "temporary";
+
+
+
+        /// <summary>
+        /// URIが AssetBundle スキームかどうか
+        /// </summary>
+        /// <param name="uri">確認するURI</param>
+        /// <returns>assetbundle スキームの場合は true を、違う場合は false を返します</returns>
+        public static bool IsAssetBundle(this Uri uri)
+        {
+            // URIが絶対パスのときのみAssetBundleスキームかどうかを返す
+            return uri.IsAbsoluteUri ? uri.Scheme == SchemeAssetBundle : false;
+        }
+
+
+        /// <summary>
+        /// URIが AssetCatalog スキームかどうか
+        /// </summary>
+        /// <param name="uri">確認するURI</param>
+        /// <returns>assetcatalog スキームの場合は true を、違う場合は false を返します</returns>
+        public static bool IsAssetCatalog(this Uri uri)
+        {
+            // URIが絶対パスのときのみAssetCatalogスキームかどうかを返す
+            return uri.IsAbsoluteUri ? uri.Scheme == SchemeAssetCatalog : false;
+        }
+
+
+        /// <summary>
+        /// URIが AssetDatabase スキームかどうか
+        /// </summary>
+        /// <param name="uri">確認するURI</param>
+        /// <returns>assetdatabase スキームの場合は true を、違う場合は false を返します</returns>
+        public static bool IsAssetDatabase(this Uri uri)
+        {
+            // URIが絶対パスのときのみAssetDatabaseスキームかどうかを返す
+            return uri.IsAbsoluteUri ? uri.Scheme == SchemeAssetDatabase : false;
+        }
+
+
+        /// <summary>
+        /// URIが StreammingAssets スキームかどうか
+        /// </summary>
+        /// <param name="uri">確認するURI</param>
+        /// <returns>streammingassets スキームの場合は true を、違う場合は false を返します</returns>
+        public static bool IsStreammingAssets(this Uri uri)
+        {
+            // URIが絶対パスのときのみStreammingAssetsスキームかどうかを返す
+            return uri.IsAbsoluteUri ? uri.Scheme == SchemeStreammingAssets : false;
+        }
+
+
+        /// <summary>
+        /// URIが Resources スキームかどうか
+        /// </summary>
+        /// <param name="uri">確認するURI</param>
+        /// <returns>resources スキームの場合は true を、違う場合は false を返します</returns>
+        public static bool IsResources(this Uri uri)
+        {
+            // URIが絶対パスのときのみResourcesスキームかどうかを返す
+            return uri.IsAbsoluteUri ? uri.Scheme == SchemeResources : false;
+        }
+
+
+        /// <summary>
+        /// URIが Temporary スキームかどうか
+        /// </summary>
+        /// <param name="uri">確認するURI</param>
+        /// <returns>temporary スキームの場合は true を、違う場合は false を返します</returns>
+        public static bool IsTemporary(this Uri uri)
+        {
+            // URIが絶対パスのときのみTemporaryスキームかどうかを返す
+            return uri.IsAbsoluteUri ? uri.Scheme == SchemeTemporary : false;
+        }
+    }
+}
