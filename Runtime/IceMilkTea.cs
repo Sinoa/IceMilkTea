8c80604ee24e464d01e50a4fda0d22f9f169811a
Sinoa <sinoa.dev@gmail.com>
2020-02-12 17:29:17 +0900

    お試しコミット

--- Runtime/IceMilkTea.cs
+++ Runtime/IceMilkTea.cs
@@ -1,6 +1,6 @@
 ﻿// zlib/libpng License
 //
-// Copyright (c) 2018 - 2019 Sinoa
+// Copyright (c) 2018 Sinoa
 //
 // This software is provided 'as-is', without any express or implied warranty.
 // In no event will the authors be held liable for any damages arising from the use of this software.
