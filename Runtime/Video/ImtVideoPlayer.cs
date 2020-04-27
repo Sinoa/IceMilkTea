a7d2af47e6691eb4ecc42f8bd4ecfc7ddf66c7d3
Sinoa <sinoa.dev@gmail.com>
2020-04-27 20:04:52 +0900

    namespace整理

--- Runtime/Video/ImtVideoPlayer.cs
+++ Runtime/Video/ImtVideoPlayer.cs
@@ -13,12 +13,11 @@
 // 2. Altered source versions must be plainly marked as such, and must not be misrepresented as being the original software.
 // 3. This notice may not be removed or altered from any source distribution.
 
-using System;
 using System.Collections.Generic;
-using UnityEngine;
-using UnityEngine.Video;
 using System.Linq;
+using UnityEngine;
 using UnityEngine.Experimental.Rendering;
+using UnityEngine.Video;
 
 namespace IceMilkTea.Video
 {
