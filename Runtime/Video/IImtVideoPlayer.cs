bbf801597d51406e529dae1d1cb582e33abcff33
Sinoa <sinoa.dev@gmail.com>
2020-08-05 11:45:42 +0900

    追加で必要なインターフェイスを定義

--- Runtime/Video/IImtVideoPlayer.cs
+++ Runtime/Video/IImtVideoPlayer.cs
@@ -13,10 +13,16 @@
 // 2. Altered source versions must be plainly marked as such, and must not be misrepresented as being the original software.
 // 3. This notice may not be removed or altered from any source distribution.
 
+using IceMilkTea.Core;
+
 namespace IceMilkTea.Video
 {
     public interface IImtVideoPlayer
     {
+        bool IsPlaying { get; }
+
+
+
         void Play();
 
 
@@ -36,5 +42,8 @@ namespace IceMilkTea.Video
 
 
         void RemoveEventListener(IImtVideoPlayerEventListener listener);
+
+
+        IAwaitable<bool> PrepareAsync();
     }
 }
