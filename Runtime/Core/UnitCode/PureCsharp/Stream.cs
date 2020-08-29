bbe8f0dcc3aba6c7665afa690ff1312de9f45f17
Sinoa <sinoa.dev@gmail.com>
2020-08-29 20:34:19 +0900

    ストリームに流し続けるだけでついでにCRC計算

--- Runtime/Core/UnitCode/PureCsharp/Stream.cs
+++ Runtime/Core/UnitCode/PureCsharp/Stream.cs
@@ -269,6 +269,227 @@ namespace IceMilkTea.Core
 
 
 
+    #region
+    /// <summary>
+    /// ストリームの入出力をそのままCRC64計算機に流し込むストリームクラスです
+    /// </summary>
+    /// <typeparam name="TCrc">使用するCRC64</typeparam>
+    public class Crc64Stream<TCrc> : Stream where TCrc : Crc64Base
+    {
+        // メンバ変数定義
+        private readonly TCrc crc;
+        private readonly bool leaveOpen;
+        private bool disposed;
+        private ulong hash;
+
+
+
+        #region プロパティ
+        /// <summary>
+        /// 参照ストリームの CanRead をそのまま返します
+        /// </summary>
+        public override bool CanRead => BaseStream.CanRead;
+
+
+        /// <summary>
+        /// 参照ストリームの CanSeek をそのまま返します
+        /// </summary>
+        public override bool CanSeek => BaseStream.CanSeek;
+
+
+        /// <summary>
+        /// 参照ストリームの CanWrite をそのまま返します
+        /// </summary>
+        public override bool CanWrite => BaseStream.CanWrite;
+
+
+        /// <summary>
+        /// 参照ストリームの Length をそのまま返します
+        /// </summary>
+        public override long Length => BaseStream.Length;
+
+
+        /// <summary>
+        /// 参照ストリームの Position にそのままアクセスします
+        /// </summary>
+        public override long Position { get => BaseStream.Position; set => BaseStream.Position = value; }
+
+
+        /// <summary>
+        /// このインスタンスが参照している基本ストリームを取得します
+        /// </summary>
+        public Stream BaseStream { get; private set; }
+        #endregion
+
+
+
+        #region 初期化＆解放
+        /// <summary>
+        /// Crc64Stream クラスのインスタンスを初期化します
+        /// </summary>
+        /// <param name="baseStream">CRC計算を行う対象となるストリーム</param>
+        /// <param name="crc">使用するCRCアルゴリズム</param>
+        /// <exception cref="ArgumentNullException">baseStream が null です</exception>
+        /// <exception cref="ArgumentNullException">crc が null です</exception>
+        public Crc64Stream(Stream baseStream, TCrc crc) : this(baseStream, crc, false)
+        {
+        }
+
+
+        /// <summary>
+        /// Crc64Stream クラスのインスタンスを初期化します
+        /// </summary>
+        /// <param name="baseStream">CRC計算を行う対象となるストリーム</param>
+        /// <param name="crc">使用するCRCアルゴリズム</param>
+        /// <param name="leaveOpen">このインスタンスが破棄される時に baseStream を継続して開き続ける場合は true を、破棄する場合は false を指定。既定は false です。</param>
+        /// <exception cref="ArgumentNullException">baseStream が null です</exception>
+        /// <exception cref="ArgumentNullException">crc が null です</exception>
+        public Crc64Stream(Stream baseStream, TCrc crc, bool leaveOpen)
+        {
+            // 初期化をする
+            BaseStream = baseStream ?? throw new ArgumentNullException(nameof(baseStream));
+            this.crc = crc ?? throw new ArgumentNullException(nameof(crc));
+            this.leaveOpen = leaveOpen;
+            hash = ulong.MaxValue;
+        }
+
+
+        /// <summary>
+        /// 現在使用しているリソースを破棄します
+        /// </summary>
+        /// <param name="disposing">マネージオブジェクトの破棄の場合は true それ以外は false</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposed)
+            {
+                return;
+            }
+
+
+            if (disposing && !leaveOpen)
+            {
+                BaseStream.Dispose();
+            }
+
+
+            disposed = true;
+            base.Dispose(disposing);
+        }
+        #endregion
+
+
+        #region CRC操作関数
+        /// <summary>
+        /// 現在のCRCハッシュ値を取得します
+        /// </summary>
+        /// <returns>現在のCRCハッシュ値を返します</returns>
+        public ulong GetHashData()
+        {
+            // 新しい計算済みハッシュコードの受け取り配列を生成する
+            ThrowExceptionIfObjectDisposed();
+            return hash ^ ulong.MaxValue;
+        }
+        #endregion
+
+
+        #region IO関数
+        /// <summary>
+        /// 参照ストリームの Write をそのまま呼び出します
+        /// </summary>
+        /// <param name="buffer">ストリームに書き込むデータを保持したバッファ</param>
+        /// <param name="offset">ストリームに書き込むバッファの開始位置</param>
+        /// <param name="count">ストリームに書き込むデータの数</param>
+        public override void Write(byte[] buffer, int offset, int count)
+        {
+            // ハッシュ計算に流し込んで本来の関数を呼ぶ
+            ThrowExceptionIfObjectDisposed();
+            hash = crc.Calculate(hash, buffer, offset, count);
+            BaseStream.Write(buffer, offset, count);
+        }
+
+
+        /// <summary>
+        /// 参照ストリームの Read をそのまま呼び出します
+        /// </summary>
+        /// <param name="buffer">ストリームから読みだした結果を受け取るバッファ</param>
+        /// <param name="offset">バッファが受け取る開始位置</param>
+        /// <param name="count">受け取るデータの数</param>
+        /// <returns>参照ストリームの結果をそのまま返します</returns>
+        public override int Read(byte[] buffer, int offset, int count)
+        {
+            // 本来の関数を呼んでもし読み取りサイズが有効なら
+            ThrowExceptionIfObjectDisposed();
+            var readSize = BaseStream.Read(buffer, offset, count);
+            if (readSize > 0)
+            {
+                // ハッシュ計算に流し込む
+                hash = crc.Calculate(hash, buffer, offset, readSize);
+            }
+
+
+            // 読み込んだ結果を返す
+            return readSize;
+        }
+
+
+        /// <summary>
+        /// 参照ストリームの Seek をそのまま呼び出します
+        /// </summary>
+        /// <param name="offset">origin を基準にしたシークする長さのオフセット値</param>
+        /// <param name="origin">シークする基準</param>
+        /// <returns>参照ストリームの結果をそのまま返します</returns>
+        public override long Seek(long offset, SeekOrigin origin)
+        {
+            // そのまま受け取ってそのまま返す
+            ThrowExceptionIfObjectDisposed();
+            return BaseStream.Seek(offset, origin);
+        }
+
+
+        /// <summary>
+        /// 参照ストリームの SetLength をそのまま呼び出します
+        /// </summary>
+        /// <param name="value">ストリームに設定する新しい長さ</param>
+        public override void SetLength(long value)
+        {
+            // そのまま受け取る
+            ThrowExceptionIfObjectDisposed();
+            BaseStream.SetLength(value);
+        }
+
+
+        /// <summary>
+        /// 参照ストリームの Flush を呼び出します
+        /// </summary>
+        public override void Flush()
+        {
+            // そのまま呼び出す
+            ThrowExceptionIfObjectDisposed();
+            BaseStream.Flush();
+        }
+        #endregion
+
+
+        #region 例外スロー関数
+        /// <summary>
+        /// もし、このクラスのインスタンスが破棄済みなら例外をスローします
+        /// </summary>
+        /// <exception cref="ObjectDisposedException">インスタンスが破棄済みです</exception>
+        private void ThrowExceptionIfObjectDisposed()
+        {
+            // 既に破棄済みなら
+            if (disposed)
+            {
+                // 破棄済み例外を吐く
+                throw new ObjectDisposedException(null);
+            }
+        }
+        #endregion
+    }
+    #endregion
+
+
+
     #region 監視可能なストリーム
     /// <summary>
     /// 指定したストリームの監視を可能にするストリームクラスです
